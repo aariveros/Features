a8f112521ac4eb489a2fa142479dac69a79dd907
Nicolas Castro <npcastrol@gmail.com>
2015-10-02 14:03:35 -0400

    Cambios en gp_u_set.py

--- gp_u_set.py
+++ gp_u_set.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# Toma un directorio con archivos de features sampleadas de curvas de luz
+# Toma un directorio con archivos de features sampleadas de curvas de luz.
 # Para cada set de features sampleadas calcula las gaussianas asociada a cada
 # variable. Le agrega la clase y un peso con valor 1 y arma un dataframe y lo
 # guarda como set de entrenamiento
@@ -18,41 +18,13 @@ from config import *
 def get_paths(directory):
     for dirpath, _, filenames in os.walk(directory):
         for f in filenames:
-            if '.pkl' in f:
+            if '.csv' in f:
                 yield os.path.abspath(os.path.join(dirpath, f))
 
-if len(sys.argv) == 2:
-    percentage = sys.argv[1]
-else:
-    percentage = '100'
+def calc_gaussian(file_path):
+    df = pd.read_csv(file_path)
 
-output_file_name = 'gp_u_set_' + percentage + '.csv'
-
-# Elimino features que involucran color y las CAR por temas de tiempo
-fs = FATS.FeatureSpace(Data=['magnitude', 'time', 'error'], featureList=None,
-                       excludeList=['Color', 'Eta_color', 'Q31_color',
-                       'StetsonJ','StetsonL', 'CAR_mean', 'CAR_sigma', 'CAR_tau'])
-
-linea = []
-for name in fs.featureList:
-    linea.append(name+'.l')
-    linea.append(name+'.mean')
-    linea.append(name+'.r')
-    linea.append(name+'.std')
-linea.append('weight')
-linea.append('class')
-linea = ','.join(linea) + '\n'
-f = open(TRAINING_SETS_DIR_PATH + 'GP/' + output_file_name, 'w')
-f.write(linea)
-f.close()
-
-path = LAB_PATH + 'GP_Samples/MACHO/' + percentage + '%'
-files = get_paths(path)
-
-lineas = []
-
-for f in files:
-    df = pd.read_csv(f)
+    num_feats = len(df.columns)
 
     medias = df.mean()
     std = df.std()
@@ -74,8 +46,45 @@ for f in files:
     linea.append('1.0')
     linea.append(lu.get_lc_class_name(f))
 
-    lineas.append(','.join(linea) + '\n')
+    return ','.join(linea) + '\n'
 
-f = open(output_file_name, 'a')
-f.writelines(lineas)
-f.close()
+if __name__ == '__main__':
+
+    if len(sys.argv) == 2:
+        percentage = sys.argv[1]
+    else:
+        percentage = '100'
+
+    output_file_name = 'gp_u_set_' + percentage + '.csv'
+    path = LAB_PATH + 'Samples_Features/MACHO/' + percentage + '%/'
+
+    files = get_paths(path)
+    files = [x for x in files]
+    print files
+
+    feature_list = pd.read_csv(files[0]).columns.tolist()
+
+    exclude_list = None
+
+    # Elimino features que involucran color y las CAR por temas de tiempo
+    fs = FATS.FeatureSpace(Data=['magnitude', 'time', 'error'], featureList=feature_list,
+                           excludeList=exclude_list)
+
+    linea = []
+    for name in fs.featureList:
+        linea.append(name + '.l')
+        linea.append(name + '.mean')
+        linea.append(name + '.r')
+        linea.append(name + '.std')
+    linea.append('weight')
+    linea.append('class')
+    linea = ','.join(linea) + '\n'
+    f = open(TRAINING_SETS_DIR_PATH + 'GP/' + output_file_name, 'w')
+    f.write(linea)
+    f.close()
+
+    lineas = [calc_gaussian(f) for f in files]
+
+    f = open(TRAINING_SETS_DIR_PATH + 'GP/' + output_file_name, 'a')
+    f.writelines(lineas)
+    f.close()
