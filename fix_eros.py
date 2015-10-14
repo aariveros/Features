27c3e556945fe851cfe1e6caab636a489d3e2d0c
Nicolas Castro <npcastrol@gmail.com>
2015-10-14 12:42:26 -0300

    Script para corregir errores en eros

---
+++ fix_eros.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+# --------------------------------------------------------------------------
+
+from config import *
+
+import pickle
+import os
+
+def get_paths(directory):
+    """Entrega todos los paths absolutos a objetos serializados de un directorio
+    """
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            if '.pkl' in f:
+                yield(os.path.abspath(os.path.join(dirpath, f)))
+
+
+# for p in [5,10,15,20]:
+for p in [20]:
+	percentage = str(p)
+	samples_path = LAB_PATH + 'GP_Samples/EROS/' + percentage + '%/'
+	files = get_paths(samples_path)
+
+
+	for f in files:
+		aux = open(f, 'r')
+        samples = pickle.load(aux)
+        aux.close()
+
+        errores = samples[1][0][1]
+
+        if np.all(errores == np.zeros(len(errores))):
+        	print f
