098ee390a2297ac071880fb3f6a9d6e6fd39df25
Nicolas Castro <npcastrol@gmail.com>
2015-12-28 13:53:55 -0300

    means_feats

--- macho_means_feats.py
+++ macho_means_feats.py
@@ -1,10 +1,11 @@
-# Calcula un set de features para cada curva de la base de datos macho
-# Y arma un set de entrenamiento con los valores 
+# Calcula un set de features para cada curva sacada de la media de un 
+# GP ajustado sobre las curvas de MACHO
 
 # -----------------------------------------------------------------------------
 
 import lightcurves.lc_utils as lu
 import pandas as pd
+import pickle
 import numpy as np
 import sys
 import os
@@ -16,22 +17,13 @@ import FATS
 
 from config import *
 
-# Ubicacion de las curvas
-# 0-1           Be_lc
-# 255-256       CEPH
-# 457-458       EB
-# 967-968       longperiod_lc
-# 1697-1698     microlensing_lc
-# 2862-2863     non_variables
-# 12527-12528   quasar_lc
-# 12645-12646   RRL
-
-paths = lu.get_lightcurve_paths()
-min_points = 300
-feature_values = []
-macho_ids = []
-
-# paths = paths[0:20]
+def get_paths(directory):
+    """Entrega todos los paths absolutos a objetos serializados de un directorio
+    """
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            if '.pkl' in f:
+                yield(os.path.abspath(os.path.join(dirpath, f)))
 
 if len(sys.argv) == 2:
     percentage = int(sys.argv[1])  / float(100)
@@ -40,39 +32,48 @@ else:
     print 'No se especifico el porcentaje de las curvas a utilizar'
     percentage = 1
 
-if os.path.isfile(TRAINING_SETS_DIR_PATH + 'problemas/pocos_puntos ' + str(int(percentage * 100)) + '.txt'):
-    os.remove(TRAINING_SETS_DIR_PATH + 'problemas/pocos_puntos ' + str(int(percentage * 100)) + '.txt')
+sets_dir_path = '/n/seasfs03/IACS/TSC/ncastro/GP_Means/MACHO/'
+
+paths = get_paths(sets_dir_path + str(int(percentage * 100)) + '%/')
+min_points = 300
+feature_values = []
+macho_ids = []
+
+paths = paths[0:20]
+
+if os.path.isfile(sets_dir_path + 'problemas/pocos_puntos ' + str(int(percentage * 100)) + '.txt'):
+    os.remove(sets_dir_path + 'problemas/pocos_puntos ' + str(int(percentage * 100)) + '.txt')
 
 for path in paths:
     try:
-        
-        # Descarto la banda roja por ahora
-        if not 'B.mjd' in path:
-            continue
 
         macho_id = lu.get_lightcurve_id(path)
         print 'Curva: ' + lu.get_lightcurve_id(path)
 
         macho_class = lu.get_lc_class_name(path)
-        if macho_class not in ['EB', 'Be_lc']:
-            continue
 
-        azul = lu.open_lightcurve(path)
-        azul = lu.filter_data(azul)
+        f = open(path, 'rb')
+        lc = pickle.load(f)
+        f.close()
+
+        aux = {'mag':lc[1], 'err':lc[2]}
+        lc = pd.DataFrame(aux, index=lc[0])
+
+        lc = lu.filter_data(lc)
 
         # Si la curva filtrada no tiene al menos min_points no la ocupo
-        if len(azul.index) < min_points:
-            f = open(TRAINING_SETS_DIR_PATH + 'problemas/pocos_puntos ' + str(int(percentage * 100)) + '.txt', 'a')
+        if len(lc.index) < min_points:
+            f = open(sets_dir_path + 'problemas/pocos_puntos ' + str(int(percentage * 100)) + '.txt', 'a')
             f.write(path + '\n')
             f.close()
             continue
 
         # Tomo el p% de las mediciones
-        azul = azul.iloc[0:int(len(azul) * percentage)]
+        lc = lc.iloc[0:int(len(lc) * percentage)]
 
-        t_obs = azul.index.tolist()
-        y_obs = azul['mag'].tolist()
-        err_obs = azul['err'].tolist()
+        t_obs = lc.index.tolist()
+        y_obs = lc['mag'].tolist()
+        err_obs = lc['err'].tolist()
         
         # Elimino features que involucran color y las CAR por temas de tiempo
         fs = FATS.FeatureSpace(Data=['magnitude', 'time', 'error'],
@@ -82,9 +83,8 @@ for path in paths:
         
         fs = fs.calculateFeature([y_obs, t_obs, err_obs])
 
-        clase = lu.get_lc_class_name(path)
         valores = map(lambda x: float("{0:.6f}".format(x)),fs.result(method='dict').values())
-        valores.append(clase)
+        valores.append(macho_class)
 
         feature_values.append(valores)
         macho_ids.append(macho_id)
@@ -92,7 +92,7 @@ for path in paths:
     except KeyboardInterrupt:
         raise
     except Exception, e:
-        f = open(TRAINING_SETS_DIR_PATH + 'problemas/errores ' + str(int(percentage * 100)) + '.txt', 'a')
+        f = open(sets_dir_path + 'problemas/errores ' + str(int(percentage * 100)) + '.txt', 'a')
         f.write(path + '\n')
         f.close()
         continue
@@ -103,4 +103,4 @@ df = pd.DataFrame(feature_values, columns=feature_names, index=macho_ids)
 
 df.sort(axis=1, inplace=True)
 
-df.to_csv(TRAINING_SETS_DIR_PATH + '/MACHO_Reduced/Macho reduced set ' + str(sys.argv[1]) + '.csv') 
+df.to_csv('/n/home09/ncastro/workspace/Features/sets/MACHO_Means/Macho means set ' + str(sys.argv[1]) + '.csv') 
