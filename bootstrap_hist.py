1b5476d75a71a844f43876fcff095ea79156ca59
Nicolas Castro <npcastrol@gmail.com>
2016-01-15 12:22:58 -0300

    Termino histogramas comparativos de bootstrap

--- bootstrap_hist.py
+++ bootstrap_hist.py
@@ -25,18 +25,38 @@ import utils
 # 12527-12528   quasar_lc
 # 12645-12646   RRL
 
-def graf_hist(values, real_value, file_dir):
-	plt.figure()
+def graf_hist(values, real_value, label):
+    mean = np.mean(values)
+    std = np.std(values)
+    x = np.linspace(mean - 4 * std, mean + 4 * std, 100)
+    
+    # plt.plot(x, mlab.normpdf(x, mean, std), 'k--')
 
-	mean = np.mean(values)
-	std = np.std(values)
-	x = np.linspace(mean - 4 * std, mean + 4 * std, 100)
-	plt.plot(x, mlab.normpdf(x, mean, std), 'k--')
+    n, bins, patches = plt.hist(values, 60, normed=False, histtype='bar', alpha=0.6,
+                                label=label + "%0.4f" % std)
 
-	n, bins, patches = plt.hist(values, 60, normed=1, histtype='bar', color = 'b', alpha=0.6)
-	plt.axvline(x=real_value, color = 'r', label=u'Real value')
-	plt.savefig(file_dir)
-	plt.close()
+    plt.legend()
+
+
+def calc_bootstrap(lc, kernel, sampling, feature_list):
+    samples_devs = bootstrap.GP_bootstrap(lc, kernel, sampling=sampling)
+    t_obs = samples_devs[0]
+    samples = samples_devs[1]
+    bootstrap_values = []
+
+    for s in samples:
+        y_obs = s[0]
+        err_obs = s[1]
+
+        fs = FATS.FeatureSpace(Data=['magnitude', 'time', 'error'],
+                           featureList=feature_list, excludeList=None)
+
+        fs = fs.calculateFeature([y_obs, t_obs, err_obs])
+        bootstrap_values.append(map(lambda x: float("{0:.6f}".format(x)),
+                                    fs.result(method='')))
+    return bootstrap_values
+
+file_dir = 'Resultados/Histogramas/ambos/'
 
 percentage = 0.8
 
@@ -53,9 +73,9 @@ err_obs = lc['err'].tolist()
 
 # Calculo el valor de las features para la curva completa
 feature_list = ['Amplitude', 'AndersonDarling', 'Autocor_length', 'Beyond1Std', 'Con',
-         		'Eta_e', 'LinearTrend', 'MaxSlope', 'Mean', 'Meanvariance', 'MedianAbsDev',
-         		'MedianBRP', 'PairSlopeTrend', 'PercentAmplitude', 'Q31', 'Rcs', 'Skew',
-         		'SlottedA_length', 'SmallKurtosis', 'Std', 'StetsonK', 'StetsonK_AC']
+                'Eta_e', 'LinearTrend', 'MaxSlope', 'Mean', 'Meanvariance', 'MedianAbsDev',
+                'MedianBRP', 'PairSlopeTrend', 'PercentAmplitude', 'Q31', 'Rcs', 'Skew',
+                'SlottedA_length', 'SmallKurtosis', 'Std', 'StetsonK', 'StetsonK_AC']
          
 fs = FATS.FeatureSpace(Data=['magnitude', 'time', 'error'],
                        featureList=feature_list, excludeList=None)
@@ -76,25 +96,26 @@ gp.compute(t_obs, yerr=err_obs)
 
 # bootstrap.graf_GP(lc, kernel)
 
-samples_devs = bootstrap.GP_bootstrap(lc, kernel, sampling='uniform')
-t_obs = samples_devs[0]
-samples = samples_devs[1]
-bootstrap_values = []
+equal_values = calc_bootstrap(lc, kernel, 'equal', feature_list)
+uniform_values = calc_bootstrap(lc, kernel, 'uniform', feature_list)
 
-for s in samples:
-    y_obs = s[0]
-    err_obs = s[1]
-
-    fs = FATS.FeatureSpace(Data=['magnitude', 'time', 'error'],
-                       featureList=feature_list, excludeList=None)
-
-    fs = fs.calculateFeature([y_obs, t_obs, err_obs])
-    bootstrap_values.append(map(lambda x: float("{0:.6f}".format(x)),
-    							fs.result(method='')))
-
-df = pd.DataFrame(bootstrap_values, columns=feature_list)
+equal_df = pd.DataFrame(equal_values, columns=feature_list)
+uniform_df = pd.DataFrame(uniform_values, columns=feature_list)
 
 for f_name in feature_list:
-	sampled_values = df[f_name].tolist()
-	real_value = real_values[f_name].tolist()
-	graf_hist(sampled_values, real_value, 'Resultados/Histogramas/uniform/' + f_name + '.png')
+    real_value = real_values[f_name].tolist()
+    equal_values = equal_df[f_name].tolist()
+    uniform_values = uniform_df[f_name].tolist()
+    
+    fig = plt.figure(f_name)
+    ax = fig.add_subplot(111)
+    
+    graf_hist(equal_values, real_value, 'equal std=')
+    graf_hist(uniform_values, real_value, 'uniform std=')
+
+    plt.axvline(x=real_value, color = 'r', label=u'Real value', linewidth=2.0)
+
+    plt.title(f_name)
+
+    plt.savefig(file_dir + f_name + '.png')
+    plt.close()
