4ccbb96be65d4257d5702590006480d52ca6ee07
Nicolas Castro <npcastrol@gmail.com>
2016-05-24 15:44:22 -0300

    Cambio order_eros para catalina

--- order_eros.py
+++ order_eros.py
@@ -1,44 +1,16 @@
 # coding=utf-8  
 
-# Separa las curvas de EROS en distintas carpetas según clase. Además
-# me baso en otra denominación de clases que me dio Karim
+# Separa las curvas de algun catalogo en distintas carpetas según clase
 
 # -----------------------------------------------------------------------------
 
-import pandas as pd
 import shutil
-
-import os
 import re
 
-def absoluteFilePaths(directory):
-    """Hace una busqueda recursiva por todos los directorios del directorio
-    recibido. Y retorna una lista con el path absoluto de todos los archivos
-    que encuentra.
-    """
-    for dirpath, _, filenames in os.walk(directory):
-        for f in filenames:
-            yield os.path.abspath(os.path.join(dirpath, f))
+import pandas as pd
 
-def writePathsFile(dir, label=None):
-    """Busca recursivamente los archivos de  estrellas en un directorio y 
-    escribe sus paths absolutos en un archivo.
+import lightcurves.lc_utils as lu
 
-    label: Si se le entrega un string de clase. Solo considera los archivos
-    con esa extensión
-    """
-    files = absoluteFilePaths(dir)
-
-    if label:
-        with open(label + '.txt', 'w') as archivo:
-            for f in files:
-                if '.time' in f and label in f:
-                    archivo.write(f + '\n')
-    else:
-        with open('lightcurves_paths/EROS.txt', 'w') as archivo:
-            for f in files:
-                if '.time' in f:
-                    archivo.write(f + '\n')
 
 def get_id(path):
     """Recibe un path absoluto a un archivo de una curva de luz de EROS y 
@@ -46,37 +18,41 @@ def get_id(path):
     """
     pattern = re.compile('lm.*[^.time]')
     return pattern.search(path).group()
+
+def get_new_class(clase):
+    
+    if clase in ['EA', 'B_Lyrae', 'EA_up']:
+        return 'EA'
+
+    elif clase in ['HADS', 'LADS']:
+        return 'dsct'
+
+    elif clase in ['RRab', 'RRc', 'RRd', 'Blazhko']:
+        return 'rrlyr'
+
+    elif clase in ['t2cep', 'ACEP']:
+        return 'cep'
+
+    else:
+        return clase
     
 
 if __name__ == '__main__':
 
-    writePathsFile('/n/seasfs03/IACS/TSC/ncastro/EROS')
+    catalog = 'CATALINA'
+    lc_path_file = '/Users/npcastro/workspace/Features/lightcurves_paths/'
+    lc_files = lu.get_lightcurve_paths(lc_path_file, catalog=catalog)
 
-    class_dict = {0: "Ceph_10",
-                  1: "Ceph_10_20",
-                  2: "Ceph_F",
-                  3: "EB",
-                  4: "LPV",
-                  5: "Mira_AGB_O",
-                  6: "OSARG_RGB_O",
-                  7: "SRV_AGB_C",
-                  8: "SRV_AGB_O",
-                  9: "RRL",
-                  10: "T2CEPH"
-    }
+    result_dir = '/Users/npcastro/Lab/Catalina-II/'
 
-    #lc_files = absoluteFilePaths('/Users/npcastro/Desktop/EROS/EROS')
-    #classes_df = pd.read_csv('/Users/npcastro/Desktop/EROS/karim_classes.csv', index_col=0)
+    error_count = 0
 
-    #error_count = 0
-    #for path in lc_files:
-    #    if '.time' in path:
-    #        try:
-    #            lc_id = get_id(path)
-    #            clase = class_dict[classes_df.loc[lc_id]['class']]
-    #            shutil.copy(path, '/Users/npcastro/Dropbox/EROS/' + clase + '/' + lc_id + '.time')
-    #        except KeyError:
-    #            print lc_id
-    #            error_count += 1
-
-        
+    for path in lc_files:
+       if '.csv' in path:
+           try:
+               lc_id = lu.get_lightcurve_id(path, catalog=catalog)
+               clase = get_new_class(lu.get_lightcurve_class(path, catalog=catalog))
+               shutil.copy(path, result_dir + clase )
+           except KeyError:
+               print lc_id
+               error_count += 1
